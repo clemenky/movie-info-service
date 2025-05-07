16afd0fb29a40cd577002d6da942ac2b6c022e1d
Kyle <clemenky@oregonstate.edu>
2025-05-06 23:36:59 -0400

    Milestone 1

---
+++ tmdb_client.py
@@ -0,0 +1,42 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+ACCESS_TOKEN = os.environ.get('TMDB_ACCESS_TOKEN')
+BASE_URL = 'https://api.themoviedb.org/3'
+HEADERS = {
+    'accept': 'application/json',
+    'Authorization': f'Bearer {ACCESS_TOKEN}'
+}
+
+def request(endpoint, params=None):
+    url = f'{BASE_URL}/{endpoint}'
+    try:
+        response = requests.get(url, headers=HEADERS, params=params)
+        response.raise_for_status()
+        return response.json()
+    except requests.RequestException as e:
+        print(f'[TMDB API Error] {e}')
+        return None
+
+def search_by_title(movie_title, page):
+    return request('search/movie', params={'query': movie_title, 'page': page})
+
+def get_movie_details(movie_id):
+    return request(f'movie/{movie_id}', params={'append_to_response': 'credits,release_dates'})
+
+API_ENDPOINTS = {
+    'search_by_title': {
+        'function': search_by_title,
+        'required_params': ['title', 'page']
+    },
+    'get_movie_details': {
+        'function': get_movie_details,
+        'required_params': ['movie_id']
+    }
+}
+
+__all__ = [API_ENDPOINTS]
